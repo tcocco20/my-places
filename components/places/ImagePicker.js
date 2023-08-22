eaeea7f3585291289a3477fa120924e99bdf9f67
tcocco20 <theococco@yahoo.com>
2023-08-22 01:09:45 -0700

    added camera functionality

---
+++ components/places/ImagePicker.js
@@ -0,0 +1,54 @@
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
+
+const ImagePicker = () => {
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  const verifyPermission = async () => {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant camera permissions to use this app"
+      );
+    }
+
+    return true;
+  };
+
+  const takeImageHandler = async () => {
+    const hasPermission = await verifyPermission();
+
+    if (!hasPermission) {
+      return;
+    }
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    console.log(image);
+  };
+
+  return (
+    <View>
+      <View></View>
+      <Button title="Take Image" onPress={takeImageHandler} />
+    </View>
+  );
+};
+
+export default ImagePicker;
+
+const styles = StyleSheet.create({});
